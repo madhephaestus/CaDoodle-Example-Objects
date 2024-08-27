7ba7ad6dcdddb729e2877a73d43cb5dfc6e5464c
Kevin harrington <mad.hephaestus@gmail.com>
2024-08-26 21:34:02 -0400

    Adding parameter of the sides of the measurment

--- sphere.groovy
+++ sphere.groovy
@@ -2,3 +2,24 @@ import eu.mihosoft.vrl.v3d.*
 import javafx.scene.paint.Color
 
 return new Sphere(10).toCSG().toZMin().setColor(Color.LIGHTBLUE)
+
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
+import javafx.scene.paint.Color
+
+import eu.mihosoft.vrl.v3d.parametrics.*;
+CSG getObject(){
+	if(args==null)
+		args=["Test_key_here"]
+	ArrayList<Double> options = new  ArrayList<Double> ()
+	options.addAll(Arrays.asList(3,4,5,6,7,8,9,10,11,12,13,14))
+	LengthParameter word = new LengthParameter(	args[0]+"_CaDoodle_ShereGeneration_Sides",
+										8,options)
+
+	CSG text = new Sphere(10,(int)(word.getMM()*2),(int)word.getMM()).toCSG().toZMin().setColor(Color.LIGHTBLUE)
+	CSGDatabase.saveDatabase();
+	return text
+		.setParameter(word)
+		.setRegenerate({getObject()})
+}
+return getObject()
