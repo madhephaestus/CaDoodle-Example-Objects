65d21fdd5cb7b8e402237c9a98ff35fa6cce3e7a
Kevin Harrington <kharrington@bancroftschool.org>
2024-10-23 09:55:24 -0400

    updating the cone script

--- cone.groovy
+++ cone.groovy
@@ -13,11 +13,11 @@ CSG getObject(){
 		args=["Test_key_here"]
 	ArrayList<Double> options = new  ArrayList<Double> ()
 	options.addAll(Arrays.asList(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,64))
-	LengthParameter word = new LengthParameter(	args[0]+"_CaDoodle_CylinderGeneration_Sides",
+	LengthParameter word = new LengthParameter(	args[0]+"_CaDoodle_ConeSide_Sides",
 										16,options)
-	LengthParameter top = new LengthParameter(	args[0]+"_CaDoodle_CylinderGeneration_Top",
+	LengthParameter top = new LengthParameter(	args[0]+"_CaDoodle_ConeTop_Top",
 		0,[])
-	LengthParameter bottom = new LengthParameter(	args[0]+"_CaDoodle_CylinderGeneration_Bottom",
+	LengthParameter bottom = new LengthParameter(	args[0]+"_CaDoodle_ConeBottom_Bottom",
 		10,[])
 	int getMM = (int)word.getMM()
 	CSG text =  new Cylinder(bottom.getMM(),top.getMM(),20,getMM).toCSG().toZMin().setColor(Color.PURPLE)
