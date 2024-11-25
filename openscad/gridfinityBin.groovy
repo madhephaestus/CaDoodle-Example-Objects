cf2d443facdc8bc7679a6b922737ecc8b7fd8ff6
Kevin harrington <mad.hephaestus@gmail.com>
2024-11-24 20:41:00 -0500

    Adding gridfinity bin

---
+++ openscad/gridfinityBin.groovy
@@ -0,0 +1,37 @@
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.parametrics.*
+import eu.mihosoft.vrl.v3d.parametrics.CSGDatabase;
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+
+import eu.mihosoft.vrl.v3d.CSG
+
+HashMap<String,Double> params=new HashMap<String, Double>();
+CSG getObject(){
+	if(args==null) {
+		args=["Test_key_here"]
+	}
+	ArrayList<Double> options = new  ArrayList<Double> ()
+	options.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10))
+	LengthParameter x = new LengthParameter(	args[0]+"_CaDoodle_gridfinity_X",1,options)
+	LengthParameter y = new LengthParameter(	args[0]+"_CaDoodle_gridfinity_Y",1,options)
+	LengthParameter z = new LengthParameter(	args[0]+"_CaDoodle_gridfinity_Y",6,options)
+	LengthParameter mag = new LengthParameter(	args[0]+"_CaDoodle_gridfinity_Magnets",0,[0,1])
+	
+	HashMap<String,Object> params=new HashMap<String, Object>();
+	params.put("gridx", x.getMM())
+	params.put("gridy", y.getMM())
+	params.put("gridz", z.getMM())
+	params.put("magnet_holes", mag.getMM()>0)
+	
+	
+	CSG bin=	ScriptingEngine.gitScriptRun(
+			"https://github.com/kennetek/gridfinity-rebuilt-openscad.git",
+			"gridfinity-rebuilt-bins.scad",params)
+	
+	CSGDatabase.saveDatabase()
+	return bin.setParameter(y).setParameter(x).setRegenerate({getObject()})
+}
+return getObject()
+
+ 
