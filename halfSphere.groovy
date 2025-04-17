b769484ddc4d36aab5a3777bb235ba5d6a7d819f
Kevin Harrington <mad.hephaestus@gmail.com>
2025-04-17 15:08:14 -0400

    increasing the resolutions to match cylendar

--- halfSphere.groovy
+++ halfSphere.groovy
@@ -10,7 +10,7 @@ CSG getObject(){
 	if(args==null)
 		args=["Test_key_here"]
 	ArrayList<Double> options = new  ArrayList<Double> ()
-	options.addAll(Arrays.asList(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20))
+	options.addAll(Arrays.asList(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,64))
 	LengthParameter word = new LengthParameter(	args[0]+"_CaDoodle_ShereGeneration_Sides",
 										20,options)
 
