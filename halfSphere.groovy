87e4d398733544db84e8d6ac760ecc9550297132
Kevin Harrington <mad.hephaestus@gmail.com>
2025-04-18 11:57:29 -0400

    updating the sphere and half sphere defualts to actually match cylinder

--- halfSphere.groovy
+++ halfSphere.groovy
@@ -10,11 +10,11 @@ CSG getObject(){
 	if(args==null)
 		args=["Test_key_here"]
 	ArrayList<Double> options = new  ArrayList<Double> ()
-	options.addAll(Arrays.asList(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,64))
+	options.addAll(Arrays.asList(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,64))
 	LengthParameter word = new LengthParameter(	args[0]+"_CaDoodle_ShereGeneration_Sides",
 										16,options)
 
-	CSG toCSG = new Sphere(10,(int)(word.getMM()*2),(int)word.getMM()).toCSG().rotx(90)
+	CSG toCSG = new Sphere(10,(int)(word.getMM()),(int)(word.getMM()/2)).toCSG().rotx(90)
 	toCSG=toCSG.intersect(toCSG.getBoundingBox().toZMin())
 	CSG text = toCSG.toZMin().setColor(Color.PINK)
 	CSGDatabase.saveDatabase();
--- sphere.groovy
+++ sphere.groovy
@@ -10,11 +10,11 @@ CSG getObject(){
 	if(args==null)
 		args=["Test_key_here"]
 	ArrayList<Double> options = new  ArrayList<Double> ()
-	options.addAll(Arrays.asList(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,64))
+	options.addAll(Arrays.asList(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,64))
 	LengthParameter word = new LengthParameter(	args[0]+"_CaDoodle_ShereGeneration_Sides",
-										8,options)
+										16,options)
 
-	CSG text = new Sphere(10,(int)(word.getMM()*2),(int)word.getMM()).toCSG().rotx(90).toZMin().setColor(Color.LIGHTBLUE)
+	CSG text = new Sphere(10,(int)(word.getMM()),(int)(word.getMM()/2)).toCSG().rotx(90).toZMin().setColor(Color.LIGHTBLUE)
 	CSGDatabase.saveDatabase();
 	return text
 		.setParameter(word)
