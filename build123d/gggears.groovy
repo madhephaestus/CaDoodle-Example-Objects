b992d517ea747faf1050d22bf1cd638a12c943a9
Kevin Harrington <mad.hephaestus@gmail.com>
2026-01-11 21:11:17 -0500

    setting the number of teeth

--- build123d/gggears.groovy
+++ build123d/gggears.groovy
@@ -14,12 +14,14 @@ List<CSG> getObject(){
 		args=["Test_key_here"]
 	}
 	ArrayList<Double> options = new  ArrayList<Double> ()
-	options.addAll(Arrays.asList(23,30))
+	for(int i=10;i<=100;i++) {
+		options.add(i);
+	}
 	StringParameter type = new StringParameter(csgdb,
 			args[0]+"_CaDoodle_gggears_Type","spurgear",
-			new ArrayList<String>(Arrays.asList("spurgear")))
-	LengthParameter pitch = new LengthParameter(csgdb,
-			args[0]+"_CaDoodle_gggears_Pitch",
+			new ArrayList<String>(Arrays.asList("helicalgear","spurgear")))
+	LengthParameter numTeeth = new LengthParameter(csgdb,
+			args[0]+"_CaDoodle_gggears_Teeth",
 			23,
 			options)
 
@@ -27,14 +29,14 @@ List<CSG> getObject(){
 	params.add("gggears")
 	params.add( type.getStrValue())
 	params.add("--number-of-teeth")
-	params.add(pitch.getMM())
+	params.add(numTeeth.getMM())
 	List<CSG> all=	Build123dLoader.toCSG(csgdb, params)
 
 	for(CSG bin:all) {
 		bin.setNoScale(true)
 		bin
 				.setParameter(csgdb,type)
-				.setParameter(csgdb,pitch)
+				.setParameter(csgdb,numTeeth)
 				.setRegenerate({getObject()})
 	}
 	return all
