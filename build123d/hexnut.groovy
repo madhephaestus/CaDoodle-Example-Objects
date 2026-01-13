f95ed21969ae8498281b6c335971d9a5e3907c72
Kevin Harrington <mad.hephaestus@gmail.com>
2026-01-13 10:00:53 -0500

    adding a nut generator

--- build123d/hexnut.groovy
+++ build123d/hexnut.groovy
@@ -14,31 +14,16 @@ List<CSG> getObject(){
 	if(args==null) {
 		args=["Test_key_here"]
 	}
-	ArrayList<Double> options = new  ArrayList<Double> ()
-	for(int i=10;i<=100;i++) {
-		options.add(i);
-	}
-	StringParameter type = new StringParameter(csgdb,
-			args[0]+"_CaDoodle_gggears_Type","spurgear",
-			new ArrayList<String>(Arrays.asList("helicalgear","spurgear")))
-	LengthParameter numTeeth = new LengthParameter(csgdb,
-			args[0]+"_CaDoodle_gggears_Teeth",
-			23,
-			options)
+
 
 	ArrayList<Object> params=new ArrayList< Object>();
-	params.add("gggears")
-	params.add( type.getStrValue())
-	params.add("--number-of-teeth")
-	params.add(numTeeth.getMM())
+	params.add("hexnut")
+
 	List<CSG> all=	Build123dLoader.toCSG(csgdb, params)
 
 	for(CSG bin:all) {
 		bin.setNoScale(true)
-		bin
-				.setParameter(csgdb,type)
-				.setParameter(csgdb,numTeeth)
-				.setRegenerate({getObject()})
+		bin.setRegenerate({getObject()})
 	}
 	return all
 
